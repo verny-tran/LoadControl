724491b663660e559a895b82602ac9cbf7ba58cf
Trần T. Dũng <vernytran@icloud.com>
2024-04-23 12:54:32 +0700

    Add LoadControl-Demo

---
+++ LoadControl-Demo/LoadControl-Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoadControl-Demo
+//
+//  Created by Trần T. Dũng on 23/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
