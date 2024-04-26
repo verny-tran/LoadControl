cdbe415f611e3d8308f4a12484382d100cf75577
Trần T. Dũng <vernytran@icloud.com>
2024-04-26 12:41:40 +0700

    Remove SPM platforms

--- Package.swift
+++ Package.swift
@@ -5,7 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "LoadControl",
-    platforms: [.iOS(.v12), .macOS(.v11)]
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
